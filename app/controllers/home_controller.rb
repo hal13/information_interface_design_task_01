6cd0594ce3965ab197e2e8bfb8649ea397d96a3f
hal13 <a15z8my@aiit.ac.jp>
2018-06-05 08:31:48 +0000

    before deploy heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,32 @@
 class HomeController < ApplicationController
-  before_action :get_range
+  
+  DEFAULT_NUMBER_OF_VALUES = 100
+  
+  before_action :get_range, only:[:index]
+  before_action :get_number_of_values, only:[:index]
   
   def index
-    count = 100 unless params[:count]
-    @random_values = SetRandom.get_random_values(count)
+    session[:number_of_value] = DEFAULT_NUMBER_OF_VALUES unless session[:number_of_value]
+    @number_of_value = session[:number_of_value]
+    
+    @random_values = SetRandom.get_random_values(@number_of_value)
+  end
+  
+  def get_params
+    session[:number_of_value] = params[:number_of_value].to_i
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+    end
   end
   
   private
   
   def get_range
     @range_from_to = SetRandom.get_value_from_to
+  end
+  
+  def get_number_of_values
+    @select_number_of_values = { "10" => 10, "20" => 20, "50" => 50, "100" => 100, "150" => 150, "200" => 200, "500" => 500}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post "/", to: "home#get_params"
   root 'home#index'
 
 end
